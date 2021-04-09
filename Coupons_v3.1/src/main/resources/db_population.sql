afd35f168904938bc105a95df894f094afb803e2
Daniel Sorkin <danielk.sorkin@gmail.com>
2021-04-09 11:36:16 +0300

    db_population script

---
+++ Coupons_v3.1/src/main/resources/db_population.sql
@@ -0,0 +1,6 @@
+-- create demo companies
+INSERT INTO coupons.company VALUES(null,'apple','apple@mail','pass1234');
+INSERT INTO coupons.company VALUES(null,'samsung','samsung@mail','pass1234');
+INSERT INTO coupons.company VALUES(null,'panasonic','panasonic@mail','pass1234');
+INSERT INTO coupons.company VALUES(null,'sony','sony@mail','pass1234');
+INSERT INTO coupons.company VALUES(null,'haier','haier@mail','pass1234');
